adb328871bc95eeca2971e6f86f64737e79907e2
ppby <salswnss@gmail.com>
2020-07-09 16:21:08 +0900

    Modify 'prams.username이 보이도록 수정'

--- src/WithRouterSample.js
+++ src/WithRouterSample.js
@@ -15,6 +15,12 @@ const WithRouterSample = ({ location, match, history }) => {
         value={JSON.stringify(match, null, 2)}
         rows={7}
         readOnly
+        /*
+            출력된 결과를 보면 지금은 prams가 비어있음,
+            withRouter를 사용하면 현재 자신을 보여 주고 있는 라우트 컴포넌트(현재는 Profiles)를 기준으로 match가 전달
+            Profiles를 위한 라우트를 설정할 때는 path="/profiles"라고만 입력했으므로 username 파라미터를 읽어 오지 못하는 상태임
+            -> Profiles에 지우고 Profile 컴포넌트에 넣으면 username이 보일 것임
+        */
       ></textarea>
       <button onClick={() => history.push('/')}>홈으로</button>
     </div>
