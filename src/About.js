08f68f0f7108359cd6cd9101db73aca5bef2b017
ppby <salswnss@gmail.com>
2020-07-09 14:29:53 +0900

    Setting URL 쿼리

--- src/About.js
+++ src/About.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import qs from 'qs';
 
-const About = () => {
+const About = ({ location }) => {
+  const query = qs.parse(location.search, {
+    ignoreQueryPrefix: true, // 이 설정을 통해 문자열 맨 앞의 '?' 를 생략 함
+  });
+  const showDetail = query.detail === 'true'; // 쿼리의 파싱 결과 값은 문자열이기 때문에 문자열 형태로 비교
   return (
     <div>
       <h1>소개</h1>
       <p>이 프로젝트는 리액트 라우터 기초를 실습해 보는 예제 프로젝트입니다.</p>
+      {showDetail && <p>detail 값은 true로 설정했습니다.</p>}
     </div>
   );
 };
