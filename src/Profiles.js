c0d5705938f15da30d6c1ba6041dc4df6e2f3e3f
ppby <salswnss@gmail.com>
2020-07-09 16:08:04 +0900

    Setting withRouter

--- src/Profiles.js
+++ src/Profiles.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Link } from 'react-router-dom';
 import Profile from './Profile';
+import WithRouterSample from './WithRouterSample';
 
 const Profiles = () => {
   return (
@@ -21,6 +22,7 @@ const Profiles = () => {
         render={() => <div>사용자를 선택해 주세요.</div>} // 별도로 컴포넌트를 따로 만들기 애매할 때 render를 사용 (JSX를 넣어 줌), 컴포넌트에 props를 별도로 넣어 주고 싶을 때도 사용
       />
       <Route path="/profiles/:username" component={Profile} />
+      <WithRouterSample />
     </div>
   );
 };
---
+++ src/WithRouterSample.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+
+const WithRouterSample = ({ location, match, history }) => {
+  return (
+    <div>
+      <h4>location</h4>
+      <textarea
+        value={JSON.stringify(location, null, 2)} // json 파일 들여쓰기 2칸
+        rows={7}
+        readOnly
+      ></textarea>
+      <h4>match</h4>
+      <textarea
+        value={JSON.stringify(match, null, 2)}
+        rows={7}
+        readOnly
+      ></textarea>
+      <button onClick={() => history.push('/')}>홈으로</button>
+    </div>
+  );
+};
+
+export default withRouter(WithRouterSample);
